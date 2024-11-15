b13bde2cd997d2a0d5df7b3583384923af64eceb
Rando Rostok <rrostok@malwarebytes.com>
2024-11-15 20:02:15 +0200

    added fastify and redis

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 import Fastify from 'fastify';
+import { RedisClientType, createClient } from 'redis';
 import dotenv from 'dotenv';
 import { AddressInfo } from 'net';
 dotenv.config();
 
+import priceRoutes from './routes/price.routes';
+import auth from './middlewares/auth';
+
 const app = Fastify({ logger: true });
+
+app.register(priceRoutes, { prefix: 'api/prices' });
+app.addHook('preHandler', auth);
 
 app.get('/healthcheck', async () => {
   return { status: 'OK' };
@@ -11,7 +18,15 @@ app.get('/healthcheck', async () => {
 
 async function start() {
   try {
-    await app.listen({ port: Number(process.env.PORT) || 3000 });
+    const redisClient = await createClient();
+    await redisClient.connect();
+
+    app.decorate('redis', redisClient as RedisClientType);
+
+    await app.listen({
+      port: Number(process.env.PORT) || 3000,
+      host: '0.0.0.0',
+    });
     console.log(
       `Server is running at http://localhost:${
         (app.server.address() as AddressInfo)['port']
